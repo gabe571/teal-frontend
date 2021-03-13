cf841ccbd14b8c444a18a5326481ed24ac33d316
Gabe571 <gabriel.castro1028@gmail.com>
2021-03-12 18:31:30 -0800

    form layout

--- src/components/form.js
+++ src/components/form.js
@@ -7,6 +7,17 @@ const form = () => {
 return (
     <div>
     <h2> Enter an Item </h2>
+    <form>
+       <div>
+           <input type='text' className="input" placeholder='Enter Transaction'>
+           </input>
+        </div> 
+        <input type='number' className='input' placeholder='Enter Amount'>
+        </input>
+        <div>
+            <button type='submit' className='button is-primary'> Submit </button>
+        </div>
+    </form>
     </div>
  )
 }
